9c41495706cc8010f4f04e499c1f62d78db4fdb5
GreenT <limikefd@hotmail.com>
2024-09-11 20:03:32 +0800

    添加项目文件。

---
+++ git_test/function.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+/*
+加法函数
+由fan同学完成
+并把这个函数提交到自己的Github分支上
+请将函数在function.cpp中完成
+*/
+int add(int a, int b);
+
+/*
+减法函数
+由liz同学完成
+并把这个函数提交到自己的Github分支上
+请将函数在function.cpp中完成
+*/
+int minus(int a, int b);
+
+/*
+乘法函数
+由GreenT同学完成
+并把这个函数提交到自己的分支上
+请奖函数在function.cpp完成
+*/
+int times(int a, int b);
+
